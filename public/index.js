74e8007e177be77c241064fe0da3e73cffbe7244
Abhilash <abhilashjha335@gmail.com>
2023-09-13 21:22:23 +0530

    connect backend to frontend and perform CRD

---
+++ public/index.js
@@ -0,0 +1,67 @@
+const form = document.getElementById('formBox');
+const inputVal = document.getElementById('input');
+const btn = document.getElementById('btn');
+const ul = document.querySelector('ul');
+
+let TASK_URL = 'api/v1/tasks'
+
+form.addEventListener('submit', async(e)=>{
+  e.preventDefault();
+  let name = inputVal.value;
+  await fetch(TASK_URL, {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        "name" : name
+    })
+  });
+
+  inputVal.value = '';
+
+ showTasks();
+})
+
+const showTasks = async()=>{
+    let response = await fetch(TASK_URL);
+    let json = await response.json();
+    const tasks = json.data;
+    let list = '';
+    tasks.map(item => {
+        const {name, _id: id, completed} = item
+        list += `
+          <li class="single-task ${completed && 'task-complete'}"><span class="title"> ${name}</span> <span style="display: flex"> <button class="editBtn">Edit</button> <button class="delBtn" data-id=${id} >Del</button></span></li>
+        `
+    });
+
+
+    ul.innerHTML = list;
+}
+
+ul.addEventListener('click', async(e)=>{
+    let el = e.target;
+    if(el.classList.contains('delBtn')){
+        try {
+            let id = el.dataset.id;
+            await fetch(`${TASK_URL}/${id}`, {
+                method : 'DELETE',
+                headers : {
+                    "Content-Type" : "application/json"
+                }
+            })
+
+         showTasks();
+
+        } catch (error) {
+            console.error(error);
+        }
+      
+    }
+    else{
+        console.log("something went wrong");
+    }
+})
+
+
+showTasks();
